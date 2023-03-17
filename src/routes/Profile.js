34833cbc22ee9c15e2537fb9764d1f6ff66d546f
j-kyung99 <apfwnls14@naver.com>
2023-03-17 16:20:13 +0900

    #2.6_Log_Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,17 @@
+import { authService } from "fbase";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
-
-export default Profile;
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
