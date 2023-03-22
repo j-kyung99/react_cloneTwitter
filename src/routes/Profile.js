041c32f3c0acbccf6cacc7157965272983b27f09
j-kyung99 <apfwnls14@naver.com>
2023-03-22 13:42:42 +0900

    #5.0_Get_My_Own_Nweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,26 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, dbService } from "fbase";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () => {
+export default ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", `${userObj.uid}`)
+      .orderBy("createdAt")
+      .get();
+
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
